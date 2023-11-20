3242ac9b757f7f168d769b7bfd7fd67aed9ee35b
evelyn <140679177+evelynp111@users.noreply.github.com>
2023-11-20 00:56:41 -0800

    imported main framework of vanilla weather app into framework within HTML, CSS, JS files

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
-function formatDate(date) {
+function formatDate(timestamp) {
+	let date = new Date(timestamp);
+	let hours = date.getHours();
+	if (hours < 10) {
+		hours = `0${hours}`;
+	}
+	let minutes = date.getMinutes();
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
 	let days = [
 		"Sunday",
 		"Monday",
@@ -8,90 +17,98 @@ function formatDate(date) {
 		"Friday",
 		"Saturday",
 	];
-	let dayOfWeek = days[date.getDay()];
-	let timeHour = date.getHours();
-	let timeMinutes = date.getMinutes();
-	if (timeHour < 10) {
-		timeHour = `0${timeHour}`;
-	}
-	if (timeMinutes < 10) {
-		timeMinutes = `0${timeMinutes}`;
-	}
-	return `${dayOfWeek} ${timeHour}: ${timeMinutes}`;
+	let day = days[date.getDay()];
+	return `${day} ${hours}:${minutes}`;
 }
 
-let currentDate = document.querySelector("#date-time");
-let date = new Date();
-currentDate.innerHTML = formatDate(date);
-///
+function displayTemperature(response) {
+	let cityElement = document.querySelector("#city");
+	let temperatureElement =
+		document.querySelector("#temperature");
+	let descriptionElement =
+		document.querySelector("#description");
+	let windElement = document.querySelector("#wind");
+	let humidityElement = document.querySelector("#humidity");
+	let dateElement = document.querySelector("#date");
+	let iconElement = document.querySelector("#icon");
 
-//CITY SEARCH TO CORRECTLY PULL WEATHER DATA FROM API
-function displayWeather(response) {
-	console.log(response);
-	document.querySelector("#current-city").innerHTML =
-		response.data.name;
-	document.querySelector("#current-temperature").innerHTML =
-		Math.round(response.data.main.temp);
-	document.querySelector("#wind-speed").innerHTML =
-		Math.round(response.data.wind.speed);
-	document.querySelector("#humidity").innerHTML =
-		response.data.main.humidity;
+	fahrenheitTemperature = response.data.temperature.current;
+
+	temperatureElement.innerHTML = Math.round(
+		fahrenheitTemperature
+	);
+	cityElement.innerHTML = response.data.city;
+	descriptionElement.innerHTML =
+		response.data.condition.description;
+	windElement.innerHTML = Math.round(
+		response.data.wind.speed
+	);
+	humidityElement.innerHTML =
+		response.data.temperature.humidity;
+	dateElement.innerHTML = formatDate(
+		response.data.time * 1000
+	);
+	iconElement.setAttribute(
+		"src",
+		`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+	);
+	iconElement.setAttribute(
+		"alt",
+		response.data.condition.description
+	);
 }
 
 function search(city) {
-	let apiKey = "8944afa6845bd7c413a687258d3211ef";
-	let units = "imperial";
-	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
-	axios.get(apiUrl).then(displayWeather);
+	let apiKey = "fbf3f590d8fa5cdo2b6a6d68f4tc4ef2";
+	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+	axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
 	event.preventDefault();
-	let city = document.querySelector("#city-input").value;
-	search(city);
+	let cityInputElement =
+		document.querySelector("#city-input");
+	search(cityInputElement.value);
 }
 
-let cityForm = document.querySelector("#search-form");
-cityForm.addEventListener("submit", handleSubmit);
-
-search("Los Angeles");
-
-// -----------------  BONUS HOMEWORK CHALLENGE -----------
-//   Display a fake temperature (i.e 17) in Celsius and add a link to convert it to Fahrenheit.
-//   When clicking on it, it should convert the temperature to Fahrenheit. When clicking on
-//   Celsius, it should convert it back to Celsius.
-
-//fahrenheit temp = (celsius * 9/5) + 32;
-//celsius temp = (fahrenheit − 32) × 5/9
-
-function convertToCelsius(event) {
+function displayCelsiusTemp(event) {
 	event.preventDefault();
-	let temperatureElement = document.querySelector(
-		"#current-temperature"
-	);
-	let temperature = temperatureElement.innerHTML;
+	let temperatureElement =
+		document.querySelector("#temperature");
+	//remove active calss from cels. link
+	fahrenheitLink.classList.remove("active");
+	celsiusLink.classList.add("active");
+	let celsiusTemperature =
+		((fahrenheitTemperature - 32) * 5) / 9;
 	temperatureElement.innerHTML = Math.round(
-		((temperature - 32) * 5) / 9
+		celsiusTemperature
 	);
 }
-function converToFahrenheit(event) {
+function displayFahrenheitTemp(event) {
 	event.preventDefault();
-	let temperatureElement = document.querySelector(
-		"#current-temperature"
-	);
-	let temperature = temperatureElement.innerHTML;
+	let temperatureElement =
+		document.querySelector("#temperature");
+	fahrenheitLink.classList.add("active");
+	celsiusLink.classList.remove("active");
 	temperatureElement.innerHTML = Math.round(
-		(temperature * 9) / 5 + 32
+		fahrenheitTemperature
 	);
 }
+
+let fahrenheitTemperature = null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
 let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", convertToCelsius);
+celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 let fahrenheitLink = document.querySelector(
 	"#fahrenheit-link"
 );
 fahrenheitLink.addEventListener(
 	"click",
-	converToFahrenheit
+	displayFahrenheitTemp
 );
+
+search("Los Angeles");
